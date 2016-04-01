167a3a54a7cb84647f9eabac7f10dabb94187875
Olivier Gerbé <olivier.gerbe@hec.ca>
2016-04-01 13:53:14 -0400

    css reorganzation

--- karuta/js/pages/main.js
+++ karuta/js/pages/main.js
@@ -91,79 +91,70 @@ function fill_main_page(portfolioid,role)
 				changeCss("#sub-bar .open > a", "background-color:"+portfolio_navbar_color+";border-color:"+portfolio_navbar_color+";");
 			}
 			//--------------
-			var portfolio_navbar_link_color = "#ffffff"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='portfolio-navbar-link'])",data).length>0) {
 				var portfolio_navbar_link_id = $("asmContext:has(metadata[semantictag='portfolio-navbar-link'])",data).attr("id");
-				portfolio_navbar_link_color = UICom.structure["ui"][portfolio_navbar_link_id].resource.getValue();
+				var portfolio_navbar_link_color = UICom.structure["ui"][portfolio_navbar_link_id].resource.getValue();
+				changeCss("#sub-bar a", "color:"+portfolio_navbar_link_color+";");
 			}
-			changeCss("#sub-bar a", "color:"+portfolio_navbar_link_color+";");
 			//--------------------------------
-			var portfolio_sidebar_color = "#000000"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='portfolio-sidebar'])",data).length>0) {
 				var portfolio_sidebar_id = $("asmContext:has(metadata[semantictag='portfolio-sidebar'])",data).attr("id");
-				portfolio_sidebar_color = UICom.structure["ui"][portfolio_sidebar_id].resource.getValue();
+				var portfolio_sidebar_color = UICom.structure["ui"][portfolio_sidebar_id].resource.getValue();
+				changeCss("#sidebar", "background-color:"+portfolio_sidebar_color+";");
 			}
-			changeCss("#sidebar", "background-color:"+portfolio_sidebar_color+";height:100%;");
 			//--------------------------------
-			var portfolio_sidebar_link_color = "#ffffff"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='portfolio-sidebar-link'])",data).length>0) {
 				var portfolio_sidebar_link_id = $("asmContext:has(metadata[semantictag='portfolio-sidebar-link'])",data).attr("id");
-				portfolio_sidebar_link_color = UICom.structure["ui"][portfolio_sidebar_link_id].resource.getValue();
+				var portfolio_sidebar_link_color = UICom.structure["ui"][portfolio_sidebar_link_id].resource.getValue();
+				changeCss(".sidebar-link", "color:"+portfolio_sidebar_link_color+";padding-right:9px;");
+				changeCss(".sidebar-link a", "color:"+portfolio_sidebar_link_color+";");
 			}
-			changeCss(".sidebar-link", "color:"+portfolio_sidebar_link_color+";padding-right:9px;");
-			changeCss(".sidebar-link a", "color:"+portfolio_sidebar_link_color+";");
 			//--------------------------------
-			var portfolio_sidebar_link_selected_color = "#09bbd9"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='portfolio-sidebar-link-selected'])",data).length>0) {
 				var portfolio_sidebar_link_selected_id = $("asmContext:has(metadata[semantictag='portfolio-sidebar-link-selected'])",data).attr("id");
-				portfolio_sidebar_link_selected_color = UICom.structure["ui"][portfolio_sidebar_link_selected_id].resource.getValue();								
+				var portfolio_sidebar_link_selected_color = UICom.structure["ui"][portfolio_sidebar_link_selected_id].resource.getValue();								
+				changeCss(".selected a", "color:"+portfolio_sidebar_link_selected_color+";font-weight:bold;");
+				changeCss(".sidebar-link a:hover", "color:"+portfolio_sidebar_link_selected_color+";");
+				changeCss("a.sidebar-link:hover", "color:"+portfolio_sidebar_link_selected_color+";");
 			}
-			changeCss(".selected a", "color:"+portfolio_sidebar_link_selected_color+";font-weight:bold;");
-			changeCss(".sidebar-link a:hover", "color:"+portfolio_sidebar_link_selected_color+";");
-			changeCss("a.sidebar-link:hover", "color:"+portfolio_sidebar_link_selected_color+";");
 			//--------------------------------
 			if ($("asmContext:has(metadata[semantictag='portfolio-sidebar-selected-border'])",data).length>0) {
 				var portfolio_sidebar_selected_border_id = $("asmContext:has(metadata[semantictag='portfolio-sidebar-selected-border'])",data).attr("id");
 				portfolio_sidebar_selected_border_color = UICom.structure["ui"][portfolio_sidebar_selected_border_id].resource.getValue();
-				changeCss("#sidebar .selected", "border-right:4px solid "+portfolio_sidebar_selected_border_color+";padding-right:5px;");
+				changeCss("#sidebar .selected", "border-right:4px solid "+portfolio_sidebar_selected_border_color+";");
 			}
 			//--------------------------------
-			var portfolio_sidebar_separator_color = "#08a4bf"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='portfolio-sidebar-separator'])",data).length>0) {
 				var portfolio_sidebar_separator_id = $("asmContext:has(metadata[semantictag='portfolio-sidebar-separator'])",data).attr("id");
-				portfolio_sidebar_separator_color = UICom.structure["ui"][portfolio_sidebar_separator_id].resource.getValue();								
+				var portfolio_sidebar_separator_color = UICom.structure["ui"][portfolio_sidebar_separator_id].resource.getValue();								
+				changeCss(".sidebar-item", "border-bottom:1px solid "+portfolio_sidebar_separator_color+";");
+				changeCss(".sidebar-item .sidebar-item", "border-bottom:0px solid "+portfolio_sidebar_separator_color+";");
 			}
-			changeCss(".sidebar-item", "border-bottom:1px solid "+portfolio_sidebar_separator_color+";padding-left:15px;padding-top:8px;padding-bottom:8px;");
-			changeCss(".sidebar-item .sidebar-item", "border-bottom:0px solid "+portfolio_sidebar_separator_color+";padding-top:5px;padding-bottom:5px;");
 			//--------------------------------
-			var welcome_title_color = "#ffffff"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='welcome-title-color'])",data).length>0) {
 				var welcome_title_color_id = $("asmContext:has(metadata[semantictag='welcome-title-color'])",data).attr("id");
-				welcome_title_color = UICom.structure["ui"][welcome_title_color_id].resource.getValue();
+				var welcome_title_color = UICom.structure["ui"][welcome_title_color_id].resource.getValue();
+				changeCss(".page-welcome .welcome-title", "color:"+welcome_title_color+";");
 			}
-			changeCss(".page-welcome .welcome-title", "color:"+welcome_title_color+";text-align:center;width:100%;font-size:30px;");
 			//--------------------------------
-			var welcome_line_color = "#b9da00"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='welcome-line-color'])",data).length>0) {
 				var welcome_line_color_id = $("asmContext:has(metadata[semantictag='welcome-line-color'])",data).attr("id");
-				welcome_line_color = UICom.structure["ui"][welcome_line_color_id].resource.getValue();
+				var welcome_line_color = UICom.structure["ui"][welcome_line_color_id].resource.getValue();
+				changeCss(".welcome-line", "border-bottom:1px solid "+welcome_line_color+";width:25%;margin-left:auto;margin-right:auto;");
 			}
-			changeCss(".welcome-line", "border-bottom:1px solid "+welcome_line_color+";width:25%;margin-left:auto;margin-right:auto;");
 			//--------------------------------
-			var page_title_background_color = "#e8e8e8"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='page-title-background-color'])",data).length>0) {
 				var page_title_background_color_id = $("asmContext:has(metadata[semantictag='page-title-background-color'])",data).attr("id");
-				page_title_background_color = UICom.structure["ui"][page_title_background_color_id].resource.getValue();
+				var page_title_background_color = UICom.structure["ui"][page_title_background_color_id].resource.getValue();
+				changeCss(".welcome-line,.row-node-asmRoot,.row-node-asmStructure,.row-node-asmUnit", "background-color:"+page_title_background_color+";");
+				changeCss(".row-node", "border-top:1px solid "+page_title_background_color+";");
 			}
-			changeCss(".welcome-line,.row-node-asmRoot,.row-node-asmStructure,.row-node-asmUnit", "background-color:"+page_title_background_color+";");
-			changeCss(".row-node", "border-top:1px solid "+page_title_background_color+";");
 			//--------------------------------
-			var page_title_subline_color = "#09bbd9"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='page-title-subline-color'])",data).length>0) {
 				var page_title_subline_color_id = $("asmContext:has(metadata[semantictag='page-title-subline-color'])",data).attr("id");
-				page_title_subline_color = UICom.structure["ui"][page_title_subline_color_id].resource.getValue();
+				var page_title_subline_color = UICom.structure["ui"][page_title_subline_color_id].resource.getValue();
+				changeCss(".row-node-asmRoot .title-subline,.row-node-asmStructure .title-subline,.row-node-asmUnit .title-subline", "border-bottom:1px solid "+page_title_subline_color+";");
 			}
-			changeCss(".row-node-asmRoot .title-subline,.row-node-asmStructure .title-subline,.row-node-asmUnit .title-subline", "border-bottom:1px solid "+page_title_subline_color+";");
 			//--------------------------------
 			if ($("asmContext:has(metadata[semantictag='portfolio-buttons-color'])",data).length>0) {
 				var portfolio_buttons_color_id = $("asmContext:has(metadata[semantictag='portfolio-buttons-color'])",data).attr("id");
@@ -172,27 +163,24 @@ function fill_main_page(portfolioid,role)
 				changeCss(".collapsible .glyphicon,.btn-group .button", "color:"+portfolio_buttons_color+";");
 			}
 			//--------------------------------
-			var portfolio_buttons_background_color = "#d8d8d8"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='portfolio-buttons-background-color'])",data).length>0) {
 				var portfolio_buttons_background_color_id = $("asmContext:has(metadata[semantictag='portfolio-buttons-background-color'])",data).attr("id");
-				portfolio_buttons_background_color = UICom.structure["ui"][portfolio_buttons_background_color_id].resource.getValue();
+				var portfolio_buttons_background_color = UICom.structure["ui"][portfolio_buttons_background_color_id].resource.getValue();
+				changeCss(".row-resource td.buttons,.csv-button,.pdf-button", "border:1px solid "+portfolio_buttons_background_color+";");
+				changeCss(".row-resource td.buttons,.csv-button,.pdf-button", "background:"+portfolio_buttons_background_color+";");
 			}
-			changeCss(".row-resource td.buttons,.csv-button,.pdf-button", "border:1px solid "+portfolio_buttons_background_color+";");
-			changeCss(".row-resource td.buttons,.csv-button,.pdf-button", "background:"+portfolio_buttons_background_color+";");
 			//--------------------------------
-			var portfolio_link_color = "#337ab7"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='portfolio-link-color'])",data).length>0) {
 				var portfolio_link_color_id = $("asmContext:has(metadata[semantictag='portfolio-link-color'])",data).attr("id");
-				portfolio_link_color = UICom.structure["ui"][portfolio_link_color_id].resource.getValue();
+				var portfolio_link_color = UICom.structure["ui"][portfolio_link_color_id].resource.getValue();
+				changeCss("a", "color:"+portfolio_link_color+";");
 			}
-			changeCss("a", "color:"+portfolio_link_color+";");
 			//--------------------------------
-			var portfolio_section_title_background_color = "#f2f2f2"; // --- default value
 			if ($("asmContext:has(metadata[semantictag='portfolio-section-title-background-color'])",data).length>0) {
 				var portfolio_section_title_background_color_id = $("asmContext:has(metadata[semantictag='portfolio-section-title-background-color'])",data).attr("id");
-				portfolio_section_title_background_color = UICom.structure["ui"][portfolio_section_title_background_color_id].resource.getValue();
+				var portfolio_section_title_background_color = UICom.structure["ui"][portfolio_section_title_background_color_id].resource.getValue();
+				changeCss(".row-node-asmUnitStructure", "background:"+portfolio_section_title_background_color+";");
 			}
-			changeCss(".row-node-asmUnitStructure", "background:"+portfolio_section_title_background_color+";");
 			// ========================================================================
 			if (g_display_type=="header")
 				loadLanguages(function(data) {UIFactory["Portfolio"].displayPortfolio('main-page','header',LANGCODE,g_edit);});
