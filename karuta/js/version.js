a4627559d1d5b4baf52abd4bcf2654872d875232
Olivier Gerbé <olivier.gerbe@hec.ca>
2016-12-15 09:46:56 -0500

    Version 2.2 Beta

--- karuta/js/version.js
+++
@@ -1,8 +0,0 @@
-var karuta_version = "2.1.5";
-var karuta_date = "2016-08-16";
-
-var karuta_backend_version = "?";
-var karuta_backend_date = "?";
-var karuta_fileserver_version = "?";
-var karuta_fileserver_date = "?";
-
