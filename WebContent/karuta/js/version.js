460e3519273790bf85beb03839c70069159f45e9
Olivier Gerbé <olivier.gerbe@hec.ca>
2017-06-28 15:15:16 -0400

    Keyword: .code_empty() added for test in report CSS Adjutement navbar-brand height:auto

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -1,5 +1,5 @@
-var karuta_version = "2.3beta1";
-var karuta_date = "2017-06-27";
+var karuta_version = "2.3beta2";
+var karuta_date = "2017-06-28";
 
 var karuta_backend_version = "?";
 var karuta_backend_date = "?";
