d968e0d16a8d1fd38492f08317246c5627eafa14
Nobry OUK <o.p.nobry@gmail.com>
2017-08-28 15:54:12 -0400

    Exporting to HTML:
    * Fixed running script when you open the exported portfolio.

--- WebContent/exported.js
+++ WebContent/exported.js
@@ -13,54 +13,54 @@
 	permissions and limitations under the License.
    ======================================================= */
 
-
-/// Fix welcome page
-var blocks = document.getElementsByClassName("welcome-block");
-while( blocks.length )  // Selector is re-evaluated with classList remove
+window.onload = function()
 {
-	var b = blocks[0];
-	b.classList.remove("welcome-block");
-}
-
-/// Fix other nodes
-var blocks = document.getElementsByClassName("asmnode");
-while( blocks.length )  // Selector is re-evaluated with classList remove
-{
-	var b = blocks[0];
-	b.classList.remove("asmnode");
-}
-
-
-var nodes = document.getElementsByClassName("navbar-collapse");
-for( var i=0; i<nodes.length; i++ )
-{
-	var nod = nodes[i];
-	nod.setAttribute("class", "navbar-collapse");
-}
-
-var nodes = document.getElementsByClassName("panel-collapse");
-for( var i=0; i<nodes.length; i++ )
-{
-	var nod = nodes[i];
-	nod.setAttribute("class", "panel-collapse");
+	/// Fix welcome page
+	var blocks = document.getElementsByClassName("welcome-block");
+	while( blocks.length )  // Selector is re-evaluated with classList remove
+	{
+		var b = blocks[0];
+		b.classList.remove("welcome-block");
+	}
+	
+	/// Fix other nodes
+	var blocks = document.getElementsByClassName("asmnode");
+	while( blocks.length )  // Selector is re-evaluated with classList remove
+	{
+		var b = blocks[0];
+		b.classList.remove("asmnode");
+	}
+	
+	var nodes = document.getElementsByClassName("navbar-collapse");
+	for( var i=0; i<nodes.length; i++ )
+	{
+		var nod = nodes[i];
+		nod.setAttribute("class", "navbar-collapse");
+	}
+	
+	var nodes = document.getElementsByClassName("panel-collapse");
+	for( var i=0; i<nodes.length; i++ )
+	{
+		var nod = nodes[i];
+		nod.setAttribute("class", "panel-collapse");
+	}
 }
 
 function toggleSidebarPlus(id)
 {
 };
 
-
-
 function displayPage(id)
 {
 	/// Fetch saved page
 	/// div id=
-	var div = document.getElementById("export_sidebar_"+id).firstChild;
+	var div = document.getElementById("export_sidebar_"+id);
 	/// Replace the currently displayed one
-	var pcontent = document.getElementById("main-row");
+	var content = document.querySelector("#contenu");
+	while( content.firstChild )
+		content.removeChild(content.firstChild);
 
-	var content = pcontent.querySelector("#contenu");
-	pcontent.removeChild(content);
-  pcontent.appendChild(div.cloneNode(true));
+	// Should copy the child nodes of div, but it's still OK
+	content.appendChild(div.cloneNode(true));
 };
 
