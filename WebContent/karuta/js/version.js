55010fc9e13fdb9b90a189ddfc5c54cc7d6404b8
olivier.gerbe <olivier.gerbe@hec.ca>
2018-03-17 12:49:59 -0400

    version 2.3.1

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -1,5 +1,5 @@
-var karuta_version = "2.3";
-var karuta_date = "2017-01-31";
+var karuta_version = "2.3.1";
+var karuta_date = "2018-03-17";
 
 var karuta_backend_version = "?";
 var karuta_backend_date = "?";
