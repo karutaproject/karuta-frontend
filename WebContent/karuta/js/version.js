34658ec588c8bd3649269fcb2aae282d913a14a9
olivier.gerbe <olivier.gerbe@hec.ca>
2019-02-12 16:30:36 -0500

    2.4beta5

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -1,5 +1,5 @@
-var karuta_version = "2.4.0 beta4";
-var karuta_date = "2018-11-16";
+var karuta_version = "2.4.0 beta5";
+var karuta_date = "2019-02-12";
 
 var karuta_backend_version = "?";
 var karuta_backend_date = "?";
