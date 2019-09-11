2f64075ee86142da725a00f16f0a970d6981ffe1
oliviergerbe <oliviergerbe@10.0.1.18>
2019-09-11 16:13:17 -0400

    bug fixed - update-rights in batch

--- WebContent/karuta/js/batch.js
+++ WebContent/karuta/js/batch.js
@@ -1262,6 +1262,10 @@ g_actions['update-rights'] = function updateRights(node)
 	var wr = $(node).attr("wr");
 	var dl = $(node).attr("dl");
 	var sb = $(node).attr("sb");
+	var select = $(node).attr("select");
+	var idx = select.lastIndexOf(".");
+	var treeref = select.substring(0,idx);
+	var semtag = select.substring(idx+1);
 	//------------ Target --------------------
 	var url = getTargetUrl(node);
 	//--------------------------------
