3148b70581902bd0f4e3090a97e42b32be0a3565
oliviergerbe <oliviergerbe@10.0.1.7>
2020-01-16 11:37:50 -0500

    2.4.1-b

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -1,5 +1,5 @@
-var karuta_version = "2.4.1-b7";
-var karuta_date = "2020-01-09";
+var karuta_version = "2.4.1-b8";
+var karuta_date = "2020-01-16";
 
 var karuta_backend_version = "?";
 var karuta_backend_date = "?";
