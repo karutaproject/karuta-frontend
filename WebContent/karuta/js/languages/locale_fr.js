97d4af151040c124d343c2411f935d4c24f21a10
olivier.gerbe <olivier.gerbe@hec.ca>
2020-02-10 12:54:35 -0500

    ! in get_get_resource

--- WebContent/karuta/js/languages/locale_fr.js
+++ WebContent/karuta/js/languages/locale_fr.js
@@ -8,7 +8,7 @@ karutaStr['fr']['flag-name']="France";
 karutaStr['fr']['new']="Nouvel élément";
 karutaStr['fr']['email']="Courriel";
 karutaStr['fr']['password']="Mot de passe";
-karutaStr['fr']['login']="Entrez!";
+karutaStr['fr']['login']="Connexion";
 //----------------------------------------------------
 karutaStr['fr']['yes']="Oui";
 karutaStr['fr']['no']="Non";
@@ -315,6 +315,7 @@ karutaStr['fr']['create_user']="Ajouter un usager";
 karutaStr['fr']['active_users']="Usagers actifs";
 karutaStr['fr']['inactive_users']="Usagers inactifs";
 karutaStr['fr']['temporary_users']="Usagers temporaires";
+karutaStr['fr']['empty_users']="Usagers sans portfolio";
 karutaStr['fr']['username']="Login";
 karutaStr['fr']['lastname']="Nom";
 karutaStr['fr']['firstname']="Prénom";
@@ -334,6 +335,7 @@ karutaStr['fr']['substitute']="Substitution";
 karutaStr['fr']['select_user']="Select unusager";
 karutaStr['fr']['other']="Autre";
 karutaStr['fr']['delete-temporary-users']="Supprimer tous";
+karutaStr['fr']['delete-empty-users']="Supprimer la sélection";
 
 
 //=========================================
