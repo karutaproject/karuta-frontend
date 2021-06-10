9105b75082757e9d5c2ca12ca5726f62df85ee03
oliviergerbe <oliviergerbe@imac-de-bk>
2021-06-10 12:03:23 -0400

    Beta 1

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -1,5 +1,5 @@
-var karuta_version = "3.0 beta0.95";
-var karuta_date = "2021-05-19";
+var karuta_version = "3.0 beta1";
+var karuta_date = "2021-06-10";
 
 var karuta_backend_version = "?";
 var karuta_backend_date = "?";
