d70c7ada9657e9ce44a9095e64a4ac7fe8cca098
olivier.gerbe <olivier.gerbe@hec.ca>
2021-11-18 17:27:10 -0500

    bugs fixed

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -1,5 +1,5 @@
-var karuta_version = "3.0 beta1.8";
-var karuta_date = "2021-11-12";
+var karuta_version = "3.0 beta1.9";
+var karuta_date = "2021-11-18";
 
 var karuta_backend_version = "?";
 var karuta_backend_date = "?";
