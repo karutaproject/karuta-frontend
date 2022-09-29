58f61a1e8a887f45f6e5ab8cd689d822f62aa907
olivier.gerbe <olivier.gerbe@hec.ca>
2022-09-29 12:43:17 +0200

    if get_resource values are not empty there are displayed even if not in the list of the research (inline editing)

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -1,5 +1,5 @@
-var karuta_version = "3.1 Beta3.5.1";
-var karuta_date = "2022-09-09";
+var karuta_version = "3.1 Beta3.5.2";
+var karuta_date = "2022-09-29";
 
 var karuta_backend_version = "?";
 var karuta_backend_date = "?";
