8a6677045098e825c9505d65547fd84f7053b2fa
oliviergerbe <oliviergerbe@localhost>
2023-10-04 11:42:47 -0400

    version 3.12.2

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -2,7 +2,7 @@
 var karuta_version = "@project.version@";
 var karuta_date = "@build.timestamp@";
 
-const karuta_version_eportfolium = "eportfolium-3.12.1";
+const karuta_version_eportfolium = "eportfolium-3.12.2";
 const karuta_date_eportfolium = "2023/09/19";
 
 var karuta_backend_version = "?";
