9d3480a5e3ef9cc7037906f2566202c114fd5666
olivier.gerbe <olivier.gerbe@hec.ca>
2024-02-22 16:01:42 -0500

    svg new functionality

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -2,8 +2,8 @@
 var karuta_version = "@project.version@";
 var karuta_date = "@build.timestamp@";
 
-const karuta_version_eportfolium = "eportfolium-3.25.1";
-const karuta_date_eportfolium = "2024/02/18";
+const karuta_version_eportfolium = "eportfolium-3.26.0";
+const karuta_date_eportfolium = "2024/02/22";
 
 
 var karuta_backend_version = "?";
@@ -11,3 +11,4 @@ var karuta_backend_date = "?";
 var karuta_fileserver_version = "?";
 var karuta_fileserver_date = "?";
 
+// 3.26.0 new functionnality for svg report - 2024/02/22
