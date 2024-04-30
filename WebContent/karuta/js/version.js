a4067523a0747b4abc4dd51f9b39cc4ac586b922
oliviergerbe <oliviergerbe@localhost>
2024-04-30 17:17:56 -0400

    3.49.0

--- WebContent/karuta/js/version.js
+++ WebContent/karuta/js/version.js
@@ -33,6 +33,7 @@ var karuta_fileserver_date = "?";
 // 3.44.0 Feat - new function for designer : setNoceCodeWithDate(nodeid) - 2024-04-04
 // 3.44.1 Fix  - error due to the JS function resourceCodecontains which had two definitions. test if value': is present or not - 2024-04-24
 // 3.45.0 Feat - verification of not existing code when duplicatte or instantiate - 2024-04-24
-// 3.46.0 Feat - userrole add4d on PUT for porfolios with multi-roles - 2024-04-24
+// 3.46.0 Feat - userrole added on PUT for porfolios with multi-roles - 2024-04-24
 // 3.47.0 Feat - new jquery function for report and batch : hasChildSemtagAndResourceTextContains, hasChildSemtagAndResourceTextNotEmpty - 2024-04-29
 // 3.48.0 Feat - Report - Collapsable Section can be open or closed - 2024-04-28
+// 3.49.0 Feat - Batch - new component : for-each-group-user - 2024-04-30
